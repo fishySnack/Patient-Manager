643a7b3834be7805d4f56e526667b203cc9ced09
fishySnack <thansu2020@gmail.com>
2025-05-22 22:30:48 -0500

    making new folders and such to use http request instead

--- bill-service/src/main/java/com/pm/bill_service/grpc/BillingGrpcService.java
+++
@@ -1,8 +0,0 @@
-package com.pm.bill_service.grpc;
-
-import net.devh.boot.grpc.server.service.GrpcService;
-
-@GrpcService
-public class BillingGrpcService {
-    
-}
---
+++ bill-service/src/main/java/com/pm/bill_service/request/BillRequest.java
@@ -0,0 +1,5 @@
+package com.pm.bill_service.request;
+
+public class BillRequest {
+    String patientId;
+}
