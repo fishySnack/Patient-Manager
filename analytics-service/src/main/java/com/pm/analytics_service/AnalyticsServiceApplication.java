69385985ac7fe96ca09ed7148cb253ec19de820a
fishySnack <thansu2020@gmail.com>
2025-05-30 00:36:43 -0500

    I FINALLLY FIX THE KAFKA CONSUMER TOOK sOOOOOO LONNNNNGG

--- analytics-service/src/main/java/com/pm/analytics_service/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/com/pm/analytics_service/AnalyticsServiceApplication.java
@@ -2,7 +2,9 @@ package com.pm.analytics_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class AnalyticsServiceApplication {
 
